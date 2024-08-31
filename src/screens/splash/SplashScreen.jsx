6f5d36fe94e7209ac6a3b54e684e6cea24484abf
mateen27 <khanmateen2713@gmail.com>
2024-08-31 22:37:40 +0530

    feat: splash screen

--- src/screens/splash/SplashScreen.jsx
+++ src/screens/splash/SplashScreen.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React, { useEffect } from 'react'
 
 const SplashScreen = ({ navigation }) => {
@@ -6,16 +6,45 @@ const SplashScreen = ({ navigation }) => {
     useEffect(() => {
         setTimeout(() => {
             navigation.push('OnBoarding');
-        }, 3000)
+        }, 5000)
     }, [])
 
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style = { styles.container }>
+      <Image style = {styles.image} source={require('../../assets/images/Image5.png')} />
+      <Text style = { styles.heading }>Stay Focused!</Text>
+      <Text style = { styles.text }>Get the cup filled of your choice to stay focused and awake. Diffrent type of coffee menu, hot lottee cappucino</Text>
     </View>
   )
 }
 
 export default SplashScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ECE0D1',
+  },
+  image: {
+    width: 500,
+    height: 500,
+    resizeMode: 'contain',
+    marginBottom: 50,
+  },
+  heading: {
+    fontSize: 24, 
+    fontWeight: 'bold',
+    color: '#38220F',
+  },
+  text: {
+    textAlign: 'center',
+    marginLeft: 38, 
+    marginRight: 38,
+    marginTop: 20,
+    fontSize: 14,
+    letterSpacing: 1.2,
+    lineHeight: 25
+  }
+})
